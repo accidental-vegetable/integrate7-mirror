98082a97d9a910e42f08d109edd983b5b6cdabd9
accidental-vegetable <84548725+accidental-vegetable@users.noreply.github.com>
2021-08-20 16:55:04 +0100

    Updates to copy_tools

--- copy_tools.sh
+++ copy_tools.sh
@@ -4,18 +4,25 @@ modprobe nbd max_part=8
 
 qemu-nbd --connect=/dev/nbd0 /var/lib/libvirt/images/win7-sp1-airgapped.qcow2
 
-fdisk /dev/nbd0 -l
-
 mkdir /mnt/somepoint
+
+fdisk /dev/nbd0 -l
+# This one ("nbd0p2") is the main NTFS partition in this case.
 # mount /dev/nbd0p1 /mnt/somepoint/
 mount /dev/nbd0p2 /mnt/somepoint/
 
+# To copy AIK Tools for Integrate7 script
 export DIR_NAME="~/Win-reinstall/Integrate7_v3.40_min"
 cp -R /mnt/somepoint/Program\ Files/Windows\ AIK/Tools/amd64/oscdimg.exe $DIR_NAME/tools/amd64/
 
 cp -R /mnt/somepoint/Program\ Files/Windows\ AIK/Tools/amd64/Servicing/* $DIR_NAME/tools/amd64/DISM/
 mv $DIR_NAME/tools/amd64/DISM/Dism.exe $DIR_NAME/tools/amd64/DISM/dism.exe
 
+# To copy out of VM the resulting slipstreamed Windows ISO
+# cp /mnt/somepoint/Integrate7_v3.40_min/Win*.iso ~/ISOs/
+# OR
+# cp /mnt/somepoint/Integrate7_v3.40_min/Windows7_x64_en-US.iso ~/ISOs/
+# chown -R user:group ~/ISOs/Win*iso
 
 umount /mnt/somepoint/
 rmdir /mnt/somepoint
